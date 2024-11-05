70b008f53cf5b95e047a8309330b5cb5ec2f02dc
khomich-ivan <Ivan@Servc.co.il>
2024-11-05 23:30:20 +0100

    updated about

--- orbX-website/src/app/components/about/about.component.ts
+++ orbX-website/src/app/components/about/about.component.ts
@@ -7,6 +7,38 @@ import {SECTIONS} from "../../../variables/sections";
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
   protected readonly SECTIONS = SECTIONS;
+  teamMembers: TeamMember[] = [
+    {
+      name: 'Yurii',
+      position: 'Chief technology officer',
+      photo: 'About_Photo_Yurii.png',
+      description: 'The technical force behind our platform, with expertise in software and game development.'
+    },
+    {
+      name: 'Roman',
+      position: 'Chief executive officer',
+      photo: 'About_Photo_Roman.png',
+      description: 'Unites our creative and strategic directions, overseeing our 3D modeling and photogrammetry processes.'
+    },
+    {
+      name: 'Ivan',
+      position: 'Platform and operations manager',
+      photo: 'About_Photo_Ivan.png',
+      description: 'Ensures seamless platform integration, focusing on optimization and user experience for global audiences.'
+    },
+    // {
+    //   name: 'Juliana',
+    //   position: 'Art exhibition curator',
+    //   photo: undefined,
+    //   description: 'Ensures seamless platform integration, focusing on optimization and user experience for global audiences.'
+    // }
+  ];
+}
+
+interface TeamMember {
+  name: string;
+  position: string;
+  photo: string | undefined;
+  description: string;
 }
