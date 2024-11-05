9835992bb9cc76fc5d87cad254598cba39ab071c
khomich-ivan <Ivan@Servc.co.il>
2024-11-05 21:54:39 +0100

    changed use cases

--- orbX-website/src/app/components/use-cases/use-cases.component.ts
+++ orbX-website/src/app/components/use-cases/use-cases.component.ts
@@ -14,6 +14,15 @@ import {SECTIONS} from "../../../variables/sections";
   styleUrl: './use-cases.component.scss'
 })
 export class UseCasesComponent {
-
   protected readonly SECTIONS = SECTIONS;
+  useCasesCards: UseCaseCard[] = [
+    { title: 'Digitalization of Exhibitions', imgName: 'usecase_icon_01.png', description: 'We transform exhibitions into engaging digital experiences by capturing the physical space and enhancing it with interactive, gamified elements.' },
+    { title: 'Dynamic <br> Learning', imgName: 'usecase_icon_02.png', description: 'We turn complex school topics into engaging, interactive games developed with teachers to reinforce learning.' },
+    { title: 'Metaverse of Education and Art', imgName: 'usecase_icon_03.png', description: 'We build a unified platform that hosts games and exhibitions in a centralized database, accessible across various devices.' }
+  ];
+}
+interface UseCaseCard {
+  title: string;
+  imgName: string;
+  description: string;
 }
