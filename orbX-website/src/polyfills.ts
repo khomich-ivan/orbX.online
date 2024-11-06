2075fdcb8e76dba6bb091bc03375a9682631738c
khomich-ivan <Ivan@Servc.co.il>
2024-11-06 23:06:15 +0100

    updated npm; added polyfills.ts; added .browserslistrc

---
+++ orbX-website/src/polyfills.ts
@@ -0,0 +1,13 @@
+// Import these additional polyfills at the top of polyfills.ts
+
+// For supporting ES6+ features on older browsers
+import 'core-js/es/array';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/promise';
+import 'core-js/es/symbol';
+import 'core-js/stable';
+
+// For supporting async/await on older browsers
+import 'regenerator-runtime/runtime';
+
